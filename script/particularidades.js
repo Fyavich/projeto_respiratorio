4de1e05346c0e7db926012dc7694fe36119a2e44
Fyavich <62184857+Fyavich@users.noreply.github.com>
2024-07-10 11:02:27 -0300

    ATT 10/07/2024

--- script/particularidades.js
+++ script/particularidades.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('data/particularidades.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
 
         const sugestoes = Object.keys(data);
         const sugestoesContainer = document.getElementById('sugestoes');
         const buscar = document.getElementById('search');
+        var planoSelecionado = '';
         
         function showSuggestions(value){
             sugestoesContainer.innerHTML = '';
@@ -14,21 +14,42 @@ document.addEventListener('DOMContentLoaded', function() {
                 const filteredItems = sugestoes.filter(item => item.toLowerCase().includes(value.toLowerCase()));
                 filteredItems.forEach(item => {
                     const sugestElement = document.createElement('div');
-                    sugestElement.classList.add('sugestoes');
+                    sugestElement.classList.add('selected');
                     sugestElement.textContent = item;
-                    sugestElement.onclick = () => {
+                    sugestElement.onclick = (e) => {
+                        e.stopPropagation();
                         buscar.value = item;
+                        planoSelecionado = buscar.value;
                         sugestoesContainer.innerHTML = '';
+
+                        const sol_hc = document.getElementById('sol-hc');
+                        sol_hc.innerHTML = data[buscar.value]["SOL_HC"];
+
+                        const sol_plan = document.getElementById('sol-plan');
+                        sol_plan.innerHTML = data[buscar.value]["SOL_PLAN"];
+                                        
+
                     };
+                    sugestoesContainer.classList.remove('hidden');
                     sugestoesContainer.appendChild(sugestElement);
                 });
+            }else{
+                sugestoesContainer.classList.add('hidden');
             }
         };
 
         buscar.addEventListener('input', (e) => {
             showSuggestions(e.target.value);
         });
-        
+
+        document.addEventListener('click', (e) => {
+            if (!sugestoesContainer.contains(e.target) && !buscar.contains(e.target)) {
+                sugestoesContainer.classList.add('hidden');
+            }
+        });
+
+
+
     })
     .catch(error => console.error('Error loading JSON:', error));
 })
