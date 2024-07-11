af77084efe3e1605c01958c4153eeb49d5e06fb4
Fyavich <62184857+Fyavich@users.noreply.github.com>
2024-07-11 11:43:26 -0300

    ATT 11/07/2024

--- script/diretrizes.js
+++ script/diretrizes.js
@@ -7,46 +7,67 @@ document.addEventListener('DOMContentLoaded', function() {
             const filial = document.getElementById('filial');
 
             document.addEventListener('change', function(selected){
-                switch(filial.value){
-                    case 'GUIDO':
-                        document.getElementById('resp-comercialHC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['HC']['Resp'];
-                        document.getElementById('tel-resp-comercialHC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['HC']['Tel'];
-                        
-                        document.getElementById('resp-comercialIC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['IC']['Resp'];
-                        document.getElementById('tel-resp-comercialIC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['IC']['Tel'];
-                        
-                        document.getElementById('resp-comercialOP-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['OP']['Resp'];
-                        document.getElementById('tel-resp-comercialOP-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['OP']['Tel'];                     
-                        
-                        document.getElementById('resp-operacional-comercial').value = data[filial.value]['Horario Comercial']['Operacional']['Resp'];
-                        document.getElementById('tel-resp-operacional-comercial').value = data[filial.value]['Horario Comercial']['Operacional']['Tel'];                     
-                        
-                        document.getElementById('resp-milao-comercial').value = data[filial.value]['Horario Comercial']['Milao']['Resp'];
-                        document.getElementById('tel-resp-milao-comercial').value = data[filial.value]['Horario Comercial']['Milao']['Tel'];                     
-                        
-                        
-                        break;
+                
+                // HORÁRIO COMERCIAL
+                document.getElementById('resp-comercialHC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['HC']['Resp'];
+                document.getElementById('tel-resp-comercialHC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['HC']['Tel'];
+                
+                document.getElementById('resp-comercialIC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['IC']['Resp'];
+                document.getElementById('tel-resp-comercialIC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['IC']['Tel'];
+                
+                document.getElementById('resp-comercialOP-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['OP']['Resp'];
+                document.getElementById('tel-resp-comercialOP-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['OP']['Tel'];                     
+                
+                document.getElementById('resp-operacional-comercial').value = data[filial.value]['Horario Comercial']['Operacional']['Resp'];
+                document.getElementById('tel-resp-operacional-comercial').value = data[filial.value]['Horario Comercial']['Operacional']['Tel'];                     
+                
+                document.getElementById('resp-milao-comercial').value = data[filial.value]['Horario Comercial']['Milao']['Resp'];
+                document.getElementById('tel-resp-milao-comercial').value = data[filial.value]['Horario Comercial']['Milao']['Tel'];                     
+                
+                // PLANTÃO
+                document.getElementById('resp-comercial-hc-plantao').value = data[filial.value]['Plantao']['Comercial']['HC']['Resp'];
+                document.getElementById('tel-resp-comercial-hc-plantao').value = data[filial.value]['Plantao']['Comercial']['HC']['Tel'];                     
+                
+                document.getElementById('resp-comercial-op-plantao').value = data[filial.value]['Plantao']['Comercial']['OP']['Resp'];
+                document.getElementById('tel-resp-comercial-op-plantao').value = data[filial.value]['Plantao']['Comercial']['OP']['Tel'];  
 
-                    case 'Rio de Janeiro':
-                        document.getElementById('resp-comercialHC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['HC']['Resp'];
-                        document.getElementById('tel-resp-comercialHC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['HC']['Tel'];
-                        
-                        document.getElementById('resp-comercialIC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['IC']['Resp'];
-                        document.getElementById('tel-resp-comercialIC-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['IC']['Tel'];
-                        
-                        document.getElementById('resp-comercialOP-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['OP']['Resp'];
-                        document.getElementById('tel-resp-comercialOP-comercial').value = data[filial.value]['Horario Comercial']['Comercial']['OP']['Tel'];
-                        
-                        document.getElementById('resp-operacional-comercial').value = data[filial.value]['Horario Comercial']['Operacional']['Resp'];
-                        document.getElementById('tel-resp-operacional-comercial').value = data[filial.value]['Horario Comercial']['Operacional']['Tel'];                     
-                        
-                        document.getElementById('resp-milao-comercial').value = data[filial.value]['Horario Comercial']['Milao']['Resp'];
-                        document.getElementById('tel-resp-milao-comercial').value = data[filial.value]['Horario Comercial']['Milao']['Tel']; 
-                        
-                        break;
-                };
+                document.getElementById('resp-comercial-ic-plantao').value = data[filial.value]['Plantao']['Comercial']['IC']['Resp'];
+                document.getElementById('tel-resp-comercial-ic-plantao').value = data[filial.value]['Plantao']['Comercial']['IC']['Tel'];  
+
+                document.getElementById('resp-operacional-plantao').value = data[filial.value]['Plantao']['Operacional']['Resp'];
+                document.getElementById('tel-resp-operacional-plantao').value = data[filial.value]['Plantao']['Operacional']['Tel'];                     
+                
+                document.getElementById('resp-milao-plantao').value = data[filial.value]['Plantao']['Milao']['Resp'];
+                document.getElementById('tel-resp-milao-plantao').value = data[filial.value]['Plantao']['Milao']['Tel'];                     
+                
             });
 
         })
         .catch(error => console.error('Error loading JSON:', error));
 });
+
+
+function CriarNovoPost(){
+    const mural = document.getElementById('mural-painel');
+
+    const recado = document.createElement('div');
+    recado.classList.add('note');
+
+    const mensagem = document.createElement('label');
+    recado.classList.add('note-message');
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'X';
+    btn.classList.add('close-btn');
+    btn.onclick = () => {
+        mural.removeChild(recado);
+    };
+    
+    var info = prompt('Digite o corpo do Recado: ');
+    mensagem.innerHTML = info;
+    
+    recado.appendChild(mensagem);
+    recado.appendChild(btn);
+
+    mural.appendChild(recado);
+};
--- script/particularidades.js
+++ script/particularidades.js
@@ -22,12 +22,65 @@ document.addEventListener('DOMContentLoaded', function() {
                         planoSelecionado = buscar.value;
                         sugestoesContainer.innerHTML = '';
 
+                        // SOLICITAÇÕES
                         const sol_hc = document.getElementById('sol-hc');
                         sol_hc.innerHTML = data[buscar.value]["SOL_HC"];
 
                         const sol_plan = document.getElementById('sol-plan');
                         sol_plan.innerHTML = data[buscar.value]["SOL_PLAN"];
-                                        
+                        
+                        // PRAZO ATENDIMENTO
+                        const prazo_imp = document.getElementById('prazo-imp');
+                        prazo_imp.innerHTML = data[buscar.value]["PRAZO_IMP"];
+
+                        const prazo_urg = document.getElementById('prazo-urg');
+                        prazo_urg.innerHTML = data[buscar.value]["PRAZO_URG"];
+
+                        const prazo_troca = document.getElementById('prazo-troca');
+                        prazo_troca.innerHTML = data[buscar.value]["PRAZO_TROCA"];
+                        console.log(prazo_troca);
+
+                        const prazo_rec = document.getElementById('prazo-rec');
+                        prazo_rec.innerHTML = data[buscar.value]["PRAZO_REC"];
+
+                        const prazo_desc = document.getElementById('prazo-desc');
+                        prazo_desc.innerHTML = data[buscar.value]["PRAZO_DESC"];
+
+                        const prazo_ret = document.getElementById('prazo-ret');
+                        prazo_ret.innerHTML = data[buscar.value]["PRAZO_RET"];
+
+                        // ACIONAR FISIO
+                        const fisio_imp_HC = document.getElementById('fisio-imp-HC');
+                        fisio_imp_HC.innerHTML = data[buscar.value]["FISIO_IMP_HC"];
+
+                        const fisio_troca_HC = document.getElementById('fisio-troca-HC');
+                        fisio_troca_HC.innerHTML = data[buscar.value]["FISIO_TROCA_HC"];
+
+                        const fisio_imp_PLAN = document.getElementById('fisio-imp-PLAN');
+                        fisio_imp_PLAN.innerHTML = data[buscar.value]["FISIO_IMP_PLAN"];
+
+                        const fisio_troca_PLAN = document.getElementById('fisio-troca-PLAN');
+                        fisio_troca_PLAN.innerHTML = data[buscar.value]["FISIO_TROCA_PLAN"];
+
+                        // OUTROS DADOS
+                        const pa_data = document.getElementById('pa_data');
+                        pa_data.innerHTML = data[buscar.value]["DATA_REG"];
+
+                        const pa_filial = document.getElementById('pa_filial');
+                        pa_filial.innerHTML = data[buscar.value]["FILIAL"];
+
+                        const pa_seg = document.getElementById('pa_seg');
+                        pa_seg.innerHTML = data[buscar.value]["SEG"];
+
+                        const pa_cod = document.getElementById('pa_cod');
+                        pa_cod.innerHTML = data[buscar.value]["COD"];
+
+                        const pa_fat_emp = document.getElementById('pa_fat_emp');
+                        pa_fat_emp.innerHTML = data[buscar.value]["FAT_EMPENHO"];
+
+                        // OBSERVAÇÃO
+                        const observacoes = document.getElementById('observacoes');
+                        observacoes.innerHTML = data[buscar.value]["OBS"];
 
                     };
                     sugestoesContainer.classList.remove('hidden');
